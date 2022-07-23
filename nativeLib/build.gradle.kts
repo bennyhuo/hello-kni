b7558e718f0c7d71dbc9b7f666b20aee0d581619
Rover12421 <rover12421@163.com>
2022-07-24 02:11:01 +0800

    update multiplatform to 1.7.10 and support multi abi

--- nativeLib/build.gradle.kts
+++ nativeLib/build.gradle.kts
@@ -1,49 +1,76 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
 import org.jetbrains.kotlin.gradle.plugin.mpp.NativeBuildType
+import org.jetbrains.kotlin.konan.target.KonanTarget.*
 
 plugins {
-    kotlin("multiplatform") version "1.3.71"
+    kotlin("multiplatform") version "1.7.10"
     id("com.android.library")
 }
 
 val jniLibDir = File(project.buildDir, arrayOf("generated", "jniLibs").joinToString(File.separator))
 
 kotlin {
-    androidNativeArm32 {
+    android()
+
+    val nativeConfigure: KotlinNativeTarget.() -> Unit = {
         binaries {
             sharedLib("knlib") {
-                if(buildType == NativeBuildType.RELEASE){
-                    linkTask.doLast {
-                        copy {
-                            from(outputFile)
-                            into(File(jniLibDir, "armeabi-v7a"))
-                        }
-                    }
+                linkTask.doLast {
+                    copy {
+                        from(outputFile)
 
-                    afterEvaluate {
-                        val preReleaseBuild by tasks.getting
-                        preReleaseBuild.dependsOn(linkTask)
+                        val typeName = if (buildType == NativeBuildType.DEBUG) "Debug" else "Release"
+                        val abiDirName = when(target.konanTarget) {
+                            ANDROID_ARM32 -> "armeabi-v7a"
+                            ANDROID_ARM64 -> "arm64-v8a"
+                            ANDROID_X86 -> "x86"
+                            ANDROID_X64 -> "x86_64"
+                            else -> "unknown"
+                        }
+
+                        into(file("$jniLibDir/$typeName/$abiDirName"))
                     }
+                }
+
+                afterEvaluate {
+                    val preBuild by tasks.getting
+                    preBuild.dependsOn(linkTask)
                 }
             }
         }
     }
-}
 
-android {
-    compileSdkVersion(26)
+    androidNativeArm32(configure = nativeConfigure)
+    androidNativeArm64(configure = nativeConfigure)
+    androidNativeX86(configure = nativeConfigure)
+    androidNativeX64(configure = nativeConfigure)
 
-    defaultConfig {
-        minSdkVersion(23)
-        targetSdkVersion(26)
+    sourceSets {
+        val androidNativeArm32Main by getting
+        val androidNativeArm64Main by getting
+        val androidNativeX86Main by getting
+        val androidNativeX64Main by getting
 
-        ndk {
-            abiFilters("armeabi-v7a")
+        val nativeMain by creating {
+            androidNativeArm32Main.dependsOn(this)
+            androidNativeArm64Main.dependsOn(this)
+            androidNativeX86Main.dependsOn(this)
+            androidNativeX64Main.dependsOn(this)
         }
     }
 
+}
+
+android {
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 23
+        targetSdk = 33
+    }
+
     sourceSets {
-        val main by getting {
-            jniLibs.srcDir(jniLibDir)
-        }
+        getByName("debug").jniLibs.srcDirs("$jniLibDir/Debug")
+        getByName("release").jniLibs.srcDirs("$jniLibDir/Release")
     }
 }
