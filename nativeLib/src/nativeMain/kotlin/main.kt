db889dfafcc820c0788e73746932f44a62dadbd4
Neo <c.min.tseng@gmail.com>
2022-12-08 02:02:30 +0800

    add buildkonfig-gradle-plugin &　prepare buildConfigField

--- nativeLib/src/nativeMain/kotlin/main.kt
+++ nativeLib/src/nativeMain/kotlin/main.kt
@@ -1,30 +1,37 @@
 import platform.android.*
 import kotlinx.cinterop.*
+import kotlin.native.CName
 
-@CName("Java_com_example_hellojni_HelloJni_sayHello")
-fun sayHello(){
+//FIXME C_PACKAGE_NAME &  C_NAME_PREFIX can use  buildkonfig-gradle-plugin ?
+const val C_PACKAGE_NAME: String = "com/example/hellojni/HelloJni"
+const val C_NAME_PREFIX: String = "Java_com_example_hellojni_HelloJni" //FIXME How use C_NAME_PREFIX_KCONFIG ?
+
+@CName(C_NAME_PREFIX+"_sayHello")
+fun sayHello() {
     __android_log_print(ANDROID_LOG_INFO.toInt(), "Kn", "Hello %s", "Native")
 }
 
-@CName("Java_com_example_hellojni_HelloJni_stringFromJNI")
+@CName(C_NAME_PREFIX+"_stringFromJNI")
 fun stringFromJNI(env: CPointer<JNIEnvVar>, thiz: jobject): jstring {
     memScoped {
         return env.pointed.pointed!!.NewStringUTF!!.invoke(env, "This is from Kotlin Native!!".cstr.ptr)!!
     }
 }
 
-@CName("Java_com_example_hellojni_HelloJni_callJava")
+@CName(C_NAME_PREFIX+"_callJava")
 fun callJava(env: CPointer<JNIEnvVar>, thiz: jobject): jstring {
     memScoped {
         val jniEnvVal = env.pointed.pointed!!
         val jclass = jniEnvVal.GetObjectClass!!.invoke(env, thiz)
-        val methodId = jniEnvVal.GetMethodID!!.invoke(env, jclass,
-                "callFromNative".cstr.ptr, "()Ljava/lang/String;".cstr.ptr)
+        val methodId = jniEnvVal.GetMethodID!!.invoke(
+            env, jclass,
+            "callFromNative".cstr.ptr, "()Ljava/lang/String;".cstr.ptr
+        )
         return jniEnvVal.CallObjectMethodA!!.invoke(env, thiz, methodId, null) as jstring
     }
 }
 
-fun sayHello2(){
+fun sayHello2() {
     __android_log_print(ANDROID_LOG_INFO.toInt(), "Kn", "Hello %s", "Native")
 }
 
@@ -35,9 +42,9 @@ fun JNI_OnLoad(vm: CPointer<JavaVMVar>, preserved: COpaquePointer): jint {
         val vmValue = vm.pointed.pointed!!
         val result = vmValue.GetEnv!!(vm, envStorage.ptr.reinterpret(), JNI_VERSION_1_6)
         __android_log_print(ANDROID_LOG_INFO.toInt(), "Kn", "JNI_OnLoad")
-        if(result == JNI_OK){
+        if (result == JNI_OK) {
             val env = envStorage.pointed!!.pointed!!
-            val jclass = env.FindClass!!(envStorage.value, "com/example/hellojni/HelloJni".cstr.ptr)
+            val jclass = env.FindClass!!(envStorage.value, C_PACKAGE_NAME.cstr.ptr)
 
             val jniMethod = allocArray<JNINativeMethod>(1)
             jniMethod[0].fnPtr = staticCFunction(::sayHello2)
@@ -45,7 +52,13 @@ fun JNI_OnLoad(vm: CPointer<JavaVMVar>, preserved: COpaquePointer): jint {
             jniMethod[0].signature = "()V".cstr.ptr
             env.RegisterNatives!!(envStorage.value, jclass, jniMethod, 1)
 
-            __android_log_print(ANDROID_LOG_INFO.toInt(), "Kn", "register say hello2, %d, %d", sizeOf<CPointerVar<JNINativeMethod>>(), sizeOf<JNINativeMethod>())
+            __android_log_print(
+                ANDROID_LOG_INFO.toInt(),
+                "Kn",
+                "register say hello2, %d, %d",
+                sizeOf<CPointerVar<JNINativeMethod>>(),
+                sizeOf<JNINativeMethod>()
+            )
         }
         JNI_VERSION_1_6
     }
